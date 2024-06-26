293b94ae92bbdceef78481f3bc154288dbc72621
Jack Nordberg <jacknordberg0@gmail.com>
2024-06-26 11:30:58 -0400

    calling the api

--- src/components/PlanetInfo.tsx
+++ src/components/PlanetInfo.tsx
@@ -1,33 +1,26 @@
-import { Alert, AlertIcon, List, ListItem, Spinner } from "@chakra-ui/react";
-import usePlanet from "../hooks/usePlanet";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
 
-const PlanetInfo = () => {
-  const { data, error, isLoading } = usePlanet();
+const ApiComponent = () => {
+  const [data, setData] = useState(null);
 
-  if (isLoading) return <Spinner />;
+  useEffect(() => {
+    apiClient
+      .get("/planets?name=Earth") // Replace with your specific endpoint
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-  if (error)
-    return (
-      <Alert status="error">
-        <AlertIcon />
-        {error}
-      </Alert>
-    );
-  if (!data) return null;
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <List>
-      <ListItem>Mass (Jupiters): {data.mass}</ListItem>
-      <ListItem>Radius (Jupiters): {data.radius}</ListItem>
-      <ListItem>Average Surface temp (kelvin): {data.temperature}</ListItem>
-      <ListItem>Orbital Period (Days):: {data.period}</ListItem>
-      <ListItem>
-        Distance from Earth (Light Years): {data.distance_light_years}
-      </ListItem>
-      <ListItem>Semi Major Axis (AU): {data.semi_major_axis}</ListItem>
-      <ListItem>Host Star Temp (Kelvin): {data.host_star_temperature}</ListItem>
-      <ListItem>Host Star Mass (Sun): {data.host_star_mass}</ListItem>
-    </List>
+    <div>
+      <h1>API Data</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   );
 };
 
-export default PlanetInfo;
+export default ApiComponent;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default axios.create({
-    baseURL: 'https://api.api-ninjas.com/v1/planets?name=',
-    params: {
-        key: 'o3DypUsg/xYdjRLyLpOiqA==h9aWfdlGu1InTVUt'
-    }
-})
+const apiClient = axios.create({
+  baseURL: '/api',
+  headers: {
+    'X-Api-Key': 'o3DypUsg/xYdjRLyLpOiqA==h9aWfdlGu1InTVUt'
+  }
+});
+
+export default apiClient;
